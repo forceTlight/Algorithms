088e66c2101bfb29b08626c792922fb3e1ee5f5d
Neo(hyeong-jun.Kim) <53989167+hyeong-jun-kim@users.noreply.github.com>
2022-05-14 11:43:24 +0900

    [레벨2/백트래킹] - 양궁대회
    프로그래머스 - [레벨2/백트래킹] - 양궁대회 , 5/14

--- algorithms/programmers/양궁 대회/src/Solution.java
+++ algorithms/programmers/양궁 대회/src/Solution.java
@@ -1,46 +1,47 @@
+
+import java.util.Arrays;
+import java.util.Arrays;
+
 public class Solution {
-    static int num;
     static int max = Integer.MIN_VALUE;
-    static int[] peach;
-    static int[] score = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    static int[] answer;
-    static boolean[] visit;
+    static int[] lion;
+    static int[] answer = {-1};
     public static void main(String args[]){
-        solution(5, new int[] {2,1,1,1,0,0,0,0,0,0,0});
+        solution(9, new int[] {0,0,1,2,0,1,1,1,1,1,1});
+        //solution(5, new int[] {2,1,1,1,0,0,0,0,0,0,0});
+        System.out.println(Arrays.toString(answer));
     }
 
     static int[] solution(int n, int[] info) {
-        num = n;
-        peach = info;
-        int[] lion = new int[11];
-        //visit = new boolean[11];
-        int[] answer = {};
+        lion = new int[11];
+        combine(info, 1, n);
         return answer;
     }
-    static void combine(int idx, int cnt, int[] lion){
-        if(cnt == num){
-            getScore(lion);
-        }
-        if(Math.abs(lion[idx] - peach[idx]) > 1) { // 차이가 1 이상일 시 리턴
+    static void combine(int[] info, int cnt, int n){
+        if(cnt == n+1){
+            getScore(info);
             return;
         }
-        for(int i = idx; i < 11; i++){
+        for(int i = 0; i <= 10 && lion[i] <= info[i]; i++){
             lion[i]++;
-            combine(i+1, cnt+1, lion);
+            combine(info, cnt+1, n);
+            lion[i]--;
         }
     }
-    static boolean getScore(int[] lion){
+    static void getScore(int[] info){
         int sum = 0;
-        for(int i = 0; i < 11; i++){
-            if(lion[i] > peach[i]){
-                sum += score[i];
-            }else if(lion[i] < peach[i]){
-                sum -= score[i];
+        for(int i = 0; i < 11; i++) {
+            if (lion[i] != 0 || info[i] != 0) {
+                if (lion[i] > info[i]) {
+                    sum += 10 - i;
+                } else if (lion[i] <= info[i]) {
+                    sum -= 10 - i;
+                }
             }
         }
-        if(max < sum){
+        if(sum > 0 && max <= sum){
             max = sum;
-            answer = lion;
+            answer = lion.clone();
         }
     }
 }

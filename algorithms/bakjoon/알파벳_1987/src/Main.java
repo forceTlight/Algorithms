2770c8085e600b5e9cf835a221a7ba9bf5151c73
forceTlight <gamebox27@naver.com>
2022-05-19 14:30:18 +0900

    [Gold4/BFS] - 1987번 알파벳_메모리 초과

--- algorithms/bakjoon/알파벳_1987/src/Main.java
+++ algorithms/bakjoon/알파벳_1987/src/Main.java
@@ -4,12 +4,20 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 class Node{
+    int n = Main.n; int m = Main.m;
     int idx;
     int distance;
-    static ArrayList<Character> c_list = new ArrayList<>();
-    public Node(int idx, int distance, ArrayList<Character> c_list){
+    int visit[][] = new int[n][m];
+    ArrayList<Character> c_list;
+    public Node(int idx, int distance, ArrayList<Character> c_list, int[][] visit){
         this.idx = idx;
         this.distance = distance;
+        this.c_list = c_list;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                this.visit[i][j] = visit[i][j];
+            }
+        }
     }
     public int getIdx(){
         return idx;
@@ -20,12 +28,14 @@ class Node{
     public ArrayList<Character> getList(){
         return c_list;
     }
+    public int[][] getVisit(){
+        return visit;
+    }
 }
 
 public class Main {
     static int n, m;
     static char map[][];
-    static int visit[][];
     //static int[][] map_cnt;
     static int[] dx = {-1, 1, 0 ,0};
     static int[] dy = {0, 0, -1, 1};
@@ -35,7 +45,6 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new char[n][m];
-        visit = new int[n][m];
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(bf.readLine(), " ");
             String input = st.nextToken();
@@ -50,22 +59,27 @@ public class Main {
         Queue<Node> q = new LinkedList<>();
         ArrayList<Character> c_list = new ArrayList<>();
         c_list.add(map[0][0]);
-        q.offer(new Node(0, 0, c_list));
+        int[][] visit = new int[n][m];
+        q.offer(new Node(0, 0, c_list, visit));
         while (!q.isEmpty()) {
             Node node = q.poll();
             int a = node.getIdx();
             int b = node.getDistance();
-            ArrayList<Character> list = (ArrayList<Character>) node.getList().clone();
+            visit = node.getVisit();
+            visit[a][b] = 1;
             for (int i = 0; i < 4; i++) {
+                ArrayList<Character> list = new ArrayList<>();
+                for(char c : node.getList()){
+                    list.add(c);
+                }
                 int x = a + dx[i];
                 int y = b + dy[i];
                 if (x >= 0 && x < n && y >= 0 && y < m) {
                     char tmp_c = map[x][y];
                     if (!list.contains(tmp_c) && visit[x][y] != 1) {
                         list.add(tmp_c);
-                        q.offer(new Node(x, y, list));
-                        visit[x][y] = 1;
-                        if(max > list.size())
+                        q.offer(new Node(x, y, list, visit));
+                        if(max < list.size())
                             max = list.size();
                     }
                 }

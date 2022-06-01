24cf9ca5e489c522465ea16150f9ef38e238d230
forceTlight <gamebox27@naver.com>
2022-06-01 13:53:28 +0900

    [Gold5/구현] 톱니바퀴 Solved

--- algorithms/bakjoon/톱니바퀴_14891/src/Main.java
+++ algorithms/bakjoon/톱니바퀴_14891/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
@@ -13,7 +14,7 @@ class Gear {
     }
     public void clockwise() {
         int[] gear_tmp = gear.clone();
-            for (int i = 1; i < 7; i++) {
+            for (int i = 1; i < 8; i++) {
                 gear[i] = gear_tmp[i-1];
             }
             gear[0] = gear_tmp[7];
@@ -26,24 +27,29 @@ class Gear {
         }
         gear[7] = gear_tmp[0];
     }
-    public void checkMove(String direct, int rotate, Integer num){ // rotate : -1 - 반시계, '' 1 - 시계 방향
+    public boolean checkMove(String direct, int rotate, Integer num){ // rotate : -1 - 반시계, '' 1 - 시계 방향
         if(direct.equals("left")){
             if(num != gear[6]){
                 if(rotate == -1){
-                    clockwise();
-                }else if(rotate == 1){
                     counterClockwise();
+                    return true;
+                }else if(rotate == 1){
+                    clockwise();
+                    return true;
                 }
             }
         }else if(direct.equals("right")){
             if(num != gear[2]){
                 if(rotate == -1){
-                    clockwise();
-                }else if(rotate == 1){
                     counterClockwise();
+                    return true;
+                }else if(rotate == 1){
+                    clockwise();
+                    return true;
                 }
             }
         }
+        return false;
     }
     public int getScore(int num){
         if(gear[0] == 0){
@@ -81,15 +87,42 @@ public class Main {
 
     public static void rotate(int gn, int rotation) {
         gn = gn-1;
+        int num = 0;
+        int tmp_num = 0;
+        int tmp_rotation = rotation;
         for(int i = gn; i < 3; i++){ // 오른쪽
-            Gear gear = gearList.get(gn);
-            int num = gear.gear[2];
-            gearList.get(gn+1).checkMove("left", rotation, num);
+            if(i == gn){
+                num = gearList.get(i).gear[2];
+            }else{
+                num = tmp_num;
+            }
+            tmp_num = gearList.get(i+1).gear[2]; // 돌아가지 않은 다음 톱니바퀴 번호 저장
+            if(tmp_rotation == 1){ // 회전 반대로
+                tmp_rotation = -1;
+            }else if(tmp_rotation == -1){
+                tmp_rotation = 1;
+            }
+            if(!gearList.get(i+1).checkMove("left", tmp_rotation, num)){
+                break;
+            }
         }
+        tmp_rotation = rotation;
+        num = 0;
+        tmp_num = 0;
         for(int i = gn; i > 0; i--){ // 왼쪽
-            Gear gear = gearList.get(gn);
-            int num = gear.gear[6];
-            gearList.get(gn-1).checkMove("right", rotation, num);
+            if(i == gn){
+                num = gearList.get(i).gear[6];
+            }else{
+                num = tmp_num;
+            }
+            tmp_num = gearList.get(i-1).gear[6]; // 돌아가지 않은 다음 톱니바퀴 번호 저장
+            if(tmp_rotation == 1){ // 회전 반대로
+                tmp_rotation = -1;
+            }else if(tmp_rotation == -1){
+                tmp_rotation = 1;
+            }
+            if(!gearList.get(i-1).checkMove("right", tmp_rotation, num)) // 극이 같을 때
+                break;
         }
         if(rotation == -1){
             gearList.get(gn).counterClockwise();

fb00fcebad0a74f71ab7bfceb6233de066de18d1
forceTlight <53989167+forceTlight@users.noreply.github.com>
2022-06-05 01:14:12 +0900

    [Gold4/Union-find] 거짓말 Solved

--- algorithms/bakjoon/거짓말_1043/src/Main.java
+++ algorithms/bakjoon/거짓말_1043/src/Main.java
@@ -1,56 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
+/*
+    Union Find
+ */
 public class Main {
     static int n; // people number
     static int m; // party number
-    static int cnt = 0;
-    static HashMap<Integer, Boolean> people = new HashMap<Integer, Boolean>();
-    static int[][] party;
-    static int truth_num;
+    static int[] parent;
+    static boolean[] truth;
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        party = new int[m][n];
-        // hashmap 초기화
-        for(int i = 0; i < n; i++)
-            people.put(i+1, false);
-        st = new StringTokenizer(br.readLine(), " ");
-        truth_num = Integer.parseInt(st.nextToken());
-        for(int i = 0; i < truth_num; i++)
-            people.put(Integer.parseInt(st.nextToken()), true);
+        ArrayList<Integer>[] parties = new ArrayList[m+1];
         for(int i = 0; i < m; i++){
-            String input = br.readLine();
-            String[] inputs = input.split(" ");
-            for(int j = 1; j < inputs.length-1; j++){ // 파티 정보 담기
-                party[i][j] = Integer.parseInt(inputs[j]);
+            parties[i] = new ArrayList<>();
+        }
+        parent = new int[n+1];
+        for(int i = 1; i < parent.length; i++){
+            parent[i] = i;
+        }
+        st = new StringTokenizer(br.readLine(), " ");
+        int truth_num = Integer.parseInt(st.nextToken());
+        truth = new boolean[n+1];
+        for(int i = 0; i < truth_num; i++){
+            truth[Integer.parseInt(st.nextToken())] = true;
+        }
+        // 파티 정보 집어넣기
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int num = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < num; j++) {
+                int p = Integer.parseInt(st.nextToken());
+                parties[i].add(p);
             }
-            int n = Integer.parseInt(inputs[0]);
-            for(int j = 0; j < n; j++){
-                for(Integer p: people.keySet()){
-                    if(people.get(Integer.parseInt(inputs[j])) == true){ // 진실을 알고있는 사람이 파티에 있다면
-                        for(int k = 0; k < n; k++){ // 그 파티에 있는사람 진실을 알고있다고 변경
-                            people.put(Integer.parseInt(inputs[k+1]), true);
-                            break;
-                        }
-                    }
+            // union
+            for(int j = 0; j < parties[i].size()-1; j++){
+                int a = parties[i].get(j);
+                int b = parties[i].get(j+1);
+                if(find(a) != find(b)){
+                    union(a, b);
                 }
             }
         }
-        // 거짓말 할 수 있는 파티 수 구하기
-        for(int i = 0; i < party.length; i++){
+        // 연관관계 있는 사람 잡아내기
+        boolean[] visited = new boolean[n+1];
+        for(int i = 1; i < n+1; i++){
+            if(truth[i] && !visited[i]){
+                int root = find(i);
+                for(int j = 1; j < n+1; j++){
+                    if(find(j) == root) {
+                        truth[j] = true;
+                        visited[j] = true;
+                    }
+                 }
+            }
+        }
+        // 과장된 이야기 할 수 있는 갯수 count
+        int cnt = 0;
+        for(int i = 0; i < m; i++){
             boolean check = true;
-            for(int j = 0; j < party[0].length; j++){
-                int p = party[i][j];
-                if(p == 0)
-                    break;
-                if(people.get(p) == true){
+            for(int j = 0; j < parties[i].size(); j++){
+                int p = parties[i].get(j);
+                if(truth[p]){
                     check = false;
                     break;
                 }
@@ -59,5 +75,21 @@ public class Main {
                 cnt++;
         }
         System.out.println(cnt);
+    }
+    // 특정 원소가 속한 집합 찾기
+    public static int find(int idx){
+        if(parent[idx] == idx)
+            return idx;
+        return find(parent[idx]);
+    }
+    // 합치기
+    public static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a > b){
+            parent[a] = b;
+        }else{
+            parent[b] = a;
+        }
     }
 }

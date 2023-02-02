b0388c1c4c388562a9a4582e24b8f505f3be5278
hyeong-jun-kim <gamebox27@naver.com>
2023-02-02 22:45:11 +0900

    [Greedy] 1946_신입_사원 Solved

--- Coding/_1946_신입_사원.java
+++ Coding/_1946_신입_사원.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Main {
+public class _1946_신입_사원 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         TreeMap<Integer, Integer> map = new TreeMap<>();

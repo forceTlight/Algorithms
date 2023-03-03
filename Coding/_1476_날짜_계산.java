07571aa9cc967c732261bac551a87acc3e17b16d
hyeong-jun-kim <gamebox27@naver.com>
2023-03-03 17:00:04 +0900

    [백준/완탐] Silver5 - 날짜 계산 Solved

--- Coding/_1476_날짜_계산.java
+++ Coding/_1476_날짜_계산.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class _1476_날짜_계산 {
     static int e, s, m;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

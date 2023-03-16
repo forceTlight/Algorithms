b5e67a9196281f3188dbb5eb7bfaefea638c8e96
hyeong-jun-kim <gamebox27@naver.com>
2023-03-16 12:43:53 +0900

    [인프런] Stack 문제 풀이

---
+++ Coding/인프런/올바른_괄호.java
@@ -0,0 +1,34 @@
+package 인프런;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 올바른_괄호 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                stack.push('(');
+            }else{
+                if(stack.isEmpty()){
+                    System.out.println("NO");
+                    return;
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+
+        if(stack.isEmpty())
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
--- Coding/인프런/크레인_인형뽑기_카카오.java
+++ Coding/인프런/크레인_인형뽑기_카카오.java
@@ -1,7 +1,12 @@
-import java.io.*;
-import java.util.*;
+package 인프런;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 크레인_인형뽑기_카카오 {
     static int[][] arr;
     static int n;
     public static void main(String args[]) throws IOException {

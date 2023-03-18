a6cb877aefde205865a4513ef6cc7fc2e9c45138
hyeong-jun-kim <gamebox27@naver.com>
2023-03-18 10:05:31 +0900

    [Do-it] 우선 순위 큐, 버블 정렬 문제풀이

--- Coding/Main.java
+++ Coding/Main.java
@@ -1,14 +1,27 @@
 import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
 
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        String input = br.readLine();
+        Queue<Character> queue = new LinkedList<>();
+        for(char c : input.toCharArray())
+            queue.offer(c);
+
+        input = br.readLine();
+        for(char c : input.toCharArray()){
+            if(queue.contains(c)){
+                char subject = queue.poll();
+                if(c != subject){
+                    System.out.println("NO");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("YES");
     }
 }

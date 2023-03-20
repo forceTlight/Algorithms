7a3a6610d4ef16a5a141b2aafd91f81364c57c60
hyeong-jun-kim <gamebox27@naver.com>
2023-03-21 08:51:09 +0900

    [Do-it] Quick,Merge,Radix Sort 풀이

--- Coding/Main.java
+++ Coding/Main.java
@@ -1,38 +1,13 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            int target = Integer.parseInt(st.nextToken());
-
-            int idx = -1;
-            for(int j = 0; j < n; j++){ // 0이나 targetNumber 찾기
-                if(arr[j] == 0 || arr[j] == target){
-                    idx = j;
-                    break;
-                }
-            }
-            if(idx == -1){
-                for(int j = n-1; j > 0; j--)
-                    arr[j] = arr[j-1];
-            }else{
-                for(int j = idx; j > 0; j--)
-                    arr[j] = arr[j-1];
-            }
-
-            arr[0] = target;
-        }
-
-        for (int a : arr)
-            System.out.print(a + " ");
     }
 }

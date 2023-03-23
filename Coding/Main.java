6a049d893391c8e80ee18dde4de6e0c5882ecbde
hyeong-jun-kim <gamebox27@naver.com>
2023-03-23 11:00:07 +0900

    [Do-it] BFS 문제 풀이

--- Coding/Main.java
+++ Coding/Main.java
@@ -1,13 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static int n;
-    static int m;
-
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());

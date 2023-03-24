612c535520b4824653f508dd305cfdd191542744
hyeong-jun-kim <gamebox27@naver.com>
2023-03-24 10:43:14 +0900

    [Do-it] 이진탐색 문제 풀이

--- Coding/Main.java
+++ Coding/Main.java
@@ -1,18 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(dfs(n));
-    }
-
-    static int dfs(int n){
-        if(n == 1)
-            return 1;
-
-        return n * dfs(n - 1);
-    }
-}
+//import java.io.*;
+//
+//public class Main {
+//    public static void main(String args[]) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        int n = Integer.parseInt(br.readLine());
+//
+//    }
+//
+//    public int dfs(int n){
+//        if(n == 1)
+//            return 1;
+//        if(n == 2)
+//            return 1;
+//
+//    }
+//}

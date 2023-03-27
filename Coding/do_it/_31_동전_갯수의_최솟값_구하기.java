3d150c222c1151bafb6fbe3aca2652af13bc8e37
hyeong-jun-kim <gamebox27@naver.com>
2023-03-28 00:36:10 +0900

    [Do-it] 그리디 문제 풀이

---
+++ Coding/do_it/_31_동전_갯수의_최솟값_구하기.java
@@ -0,0 +1,31 @@
+package do_it;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _31_동전_갯수의_최솟값_구하기 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int arr[] = new int[n];
+        int k = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int cnt = 0;
+        for(int i = n - 1; i >= 0; i--){
+            if(arr[i] <= k){
+                cnt += k / arr[i];
+                k = k % arr[i];
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
--- Coding/do_it/_32_카드_정렬하기.java
+++ Coding/do_it/_32_카드_정렬하기.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package do_it;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class _32_카드_정렬하기 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());

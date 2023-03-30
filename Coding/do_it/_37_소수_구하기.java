45513dd02294e81a6ced62987fb14bdc7dd1b4e0
hyeong-jun-kim <gamebox27@naver.com>
2023-03-30 22:43:48 +0900

    [Do-it] 소수 문제 풀이

---
+++ Coding/do_it/_37_소수_구하기.java
@@ -0,0 +1,35 @@
+package do_it;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _37_소수_구하기 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[m+1];
+
+        for(int i = 2; i <= m; i++)
+            arr[i] = i;
+
+        for(int i = 2; i <= Math.sqrt(m); i++){
+            if(arr[i] == 0)
+                continue;
+
+            for(int j = i + i; j <= m; j += i){
+                arr[j] = 0;
+            }
+        }
+
+        for(int i = n; i <=m; i++){
+            if(arr[i] != 0)
+                System.out.println(arr[i]);
+        }
+    }
+}
--- Coding/do_it/_38_거의_소수.java
+++ Coding/do_it/_38_거의_소수.java
@@ -1,9 +1,11 @@
+package do_it;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public class _38_거의_소수 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());

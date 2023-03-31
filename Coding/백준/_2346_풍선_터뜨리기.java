9981607d65734be0e317cc03f64376ed9eda68e8
hyeong-jun-kim <gamebox27@naver.com>
2023-03-31 23:08:45 +0900

    [백준] 2346 풍선 터뜨리기

--- Coding/백준/_2346_풍선_터뜨리기.java
+++ Coding/백준/_2346_풍선_터뜨리기.java
@@ -1,12 +1,12 @@
+package 백준;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
+public class _2346_풍선_터뜨리기 {
     static int[] answer;
     static class Node{
         int data;

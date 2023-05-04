22427f693d39c0673b3c12eab193fd4de101072f
hyeong-jun-kim <gamebox27@naver.com>
2023-05-04 15:05:14 +0900

    [백준] 2872 우리집엔 도서관이 있어, 12904 A와 B, 14499 주사위 굴리기

--- Coding/백준/_14499_주사위_굴리기.java
+++ Coding/백준/_14499_주사위_굴리기.java
@@ -1,10 +1,12 @@
+package 백준;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
-public class Main{
+public class _14499_주사위_굴리기 {
     static class Dice{
         int x;
         int y;

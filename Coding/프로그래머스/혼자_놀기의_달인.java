1f220931962902376ac709ed3e9eeb08370b2e6c
hyeong-jun-kim <gamebox27@naver.com>
2023-06-03 17:31:11 +0900

    [프로그래머스/구현] 혼자 놀기의 달인

--- Coding/프로그래머스/혼자_놀기의_달인.java
+++ Coding/프로그래머스/혼자_놀기의_달인.java
@@ -1,13 +1,16 @@
-// 1. 각 카드에는 1 ~ 100까지의 숫자가 하나씩 적혀있음
+package 프로그래머스;// 1. 각 카드에는 1 ~ 100까지의 숫자가 하나씩 적혀있음
 // 2. 2 이상 100 이하의 자연수를 하나 정해 그 수보다 작거나 같은 숫자 카드들을 준비하고, 준비한 카드의 수만큼 작은 상자를 자를 준비하면 게임 시작가능
 // 3. 준비된 상자에 카드를 한 장씩 넣고, 상자를 무작위로 섞어 일렬로 나열함
 // 4. 상자가 일렬로 나열되면 상자가 나열된 순서에 따라 1번부터 순차적으로 증가하는 번호를 붙임
 // 5. 다음 임의의 상자를 하나 선택해 선택한 상자 안의 숫자 카드를 확인함
 // 6. 한 카드에 적힌 번호에 해당하는 상자를 열어 담긴 카드에 적힌 숫자를 확인함
 // 7. 마찬가지로, 숫자에 해당하는 번호를 가진 상자를 계속해서 열어가며, 열어야 하는 상자가 이미 열려있을 때 까지 반복
-import java.util.*;
 
-class Solution {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+class 혼자_놀기의_달인 {
     static int[] arr;
     static ArrayList<Integer> list;
     static int answer = Integer.MIN_VALUE;
